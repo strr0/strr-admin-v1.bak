6ebfb7810ebffc50571a7d3fed9293175eb07784
strr0 <38580170+strr0@users.noreply.github.com>
2023-04-17 14:13:44 +0800

    strr-admin

--- strr-admin-common/src/main/java/com/strr/config/MybatisConfig.java
+++ strr-admin-common/src/main/java/com/strr/config/MybatisConfig.java
@@ -1,21 +1,19 @@
 package com.strr.config;
 
 import com.strr.config.builder.CrudMapperScannerBuilder;
-import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
 public class MybatisConfig {
 //    @Value("${strr.base:com.strr}")
     private String basePackage = "com.strr";
 
     @Autowired
     public MybatisConfig(SqlSessionFactory sqlSessionFactory) {
-        Configuration configuration = sqlSessionFactory.getConfiguration();
-        CrudMapperScannerBuilder builder = new CrudMapperScannerBuilder(configuration);
+        CrudMapperScannerBuilder builder = new CrudMapperScannerBuilder(sqlSessionFactory.getConfiguration());
         builder.scanMapper(basePackage);
     }
 }
