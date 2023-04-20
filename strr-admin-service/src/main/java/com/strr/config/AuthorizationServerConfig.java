09fd23ddf158f4d34ed4b3d0f4b5e75725217197
strr0 <38580170+strr0@users.noreply.github.com>
2023-04-20 16:52:45 +0800

    feat:swagger配置

--- strr-admin-service/src/main/java/com/strr/config/AuthorizationServerConfig.java
+++ strr-admin-service/src/main/java/com/strr/config/AuthorizationServerConfig.java
@@ -2,21 +2,33 @@ package com.strr.config;
 
 import com.strr.config.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 /**
  * 授权服务
  */
 @Configuration
+@EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
     private final AuthenticationManager authenticationManager;
     private final CustomUserDetailsService customUserDetailsService;
     private final PasswordEncoder passwordEncoder;
+
+    @Value("${oauth2.client}")
+    private String client;
+
+    @Value("${oauth2.secret}")
+    private String secret;
+
+    @Value("${oauth2.scopes}")
+    private String[] scopes;
 
     @Autowired
     public AuthorizationServerConfig(AuthenticationManager authenticationManager,
@@ -30,10 +42,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("STRR_CLIENT")
-                .secret(passwordEncoder.encode("STRR_SECRET"))
+                .withClient(client)
+                .secret(passwordEncoder.encode(secret))
                 .authorizedGrantTypes("refresh_token", "password", "client_credentials")
-                .scopes("web");
+                .scopes(scopes);
     }
 
     @Override
--- strr-admin-service/src/main/java/com/strr/config/ResourceServerConfig.java
+++ strr-admin-service/src/main/java/com/strr/config/ResourceServerConfig.java
@@ -4,6 +4,7 @@ import com.strr.base.model.Result;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 import java.io.PrintWriter;
@@ -12,10 +13,13 @@ import java.io.PrintWriter;
  * 资源服务
  */
 @Configuration
+@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                // swagger-ui
+                .antMatchers("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs", "/v3/api-docs", "/webjars/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
