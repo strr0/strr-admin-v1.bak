08a29802a4731e80821109c2fb7fcf4c03b23912
strr0 <38580170+strr0@users.noreply.github.com>
2023-04-20 16:58:27 +0800

    feat:多环境配置

--- strr-admin-demo/src/main/java/com/strr/DemoApplication.java
+++ strr-admin-demo/src/main/java/com/strr/DemoApplication.java
@@ -2,12 +2,8 @@ package com.strr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
-@EnableAuthorizationServer
-@EnableResourceServer
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
